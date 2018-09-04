eb1bf199c0dea50b747a2bb6877c6c83ebb2167b
vinsonly <vinsonly@live.ca>
2018-09-04 16:35:31 -0700

    added new server api routes for post and orders

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,11 @@
 const users = require('./users');
 const posts = require('./posts');
+const orders = require('./orders');
 const auth = require('./auth');
 
 module.exports = {
     users,
     posts,
+    orders,
     auth
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@
 
 const usersController = require('../controllers').users
 const postsController = require('../controllers').posts
+const ordersController = require('../controllers').orders
 const authController = require('../controllers').auth
 
 module.exports = (app) => {
@@ -20,6 +21,16 @@ module.exports = (app) => {
     app.post('/api/post/update', postsController.update);
     app.post('/api/post/delete', postsController.delete);
     app.get('/api/post/:id', postsController.findById);
+    app.get('/api/post/seller/:id', postsController.getSellerPosts);
+
+    //orders
+    app.post('/api/order', ordersController.create);
+    app.get('/api/orders', ordersController.read);
+    app.post('/api/order/update', ordersController.update);
+    app.post('/api/order/delete', ordersController.delete);
+    app.get('/api/order/:id', ordersController.findById);
+    app.get('/api/orders/buyer/:id', ordersController.getBuyerOrders);
+    app.get('/api/orders/seller/:id', ordersController.getSellerOrders);
 
     app.post('/api/login', authController.login);
 
